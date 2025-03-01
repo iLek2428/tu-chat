ea292cacc60ad0fa01ae1b6f3be7262262721f4b
Damrongsak Sattabut <me@dmg.st>
2025-03-01 11:41:18 +0700

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'Thammasat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'Thammasat';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'Thammasat University';
 
 export const BRANDING_URL = {
   help: undefined,
